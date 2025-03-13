c4edd5bee050a657f19ff9736e811f62ee688689
TecnoBase <75029624+TecnoBase@users.noreply.github.com>
2025-03-13 10:58:49 +0100

    first commit

--- src/javafxapplication1/JavaFXApplication1.java
+++ src/javafxapplication1/JavaFXApplication1.java
@@ -6,7 +6,6 @@ package javafxapplication1;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -17,20 +16,23 @@ import javafx.stage.Stage;
  * @author TecnoBase
  */
 public class JavaFXApplication1 extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
+        //button section
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
         btn.setOnAction((ActionEvent event) -> {
             System.out.println("Hello World!");
         });
-        
+        //end button section
+
+        //buid the pane
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        
+
         Scene scene = new Scene(root, 300, 250);
-        
+
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -42,5 +44,5 @@ public class JavaFXApplication1 extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
